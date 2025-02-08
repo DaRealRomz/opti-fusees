1b32d795e3fe01db46c302b6ea59caf66dcc7268
Clément Gauthier <57112933+clem2004@users.noreply.github.com>
2025-02-08 17:02:31 -0500

    ajout des molécules dans l'éditeur

--- src/components/SelectCarburant.tsx
+++ src/components/SelectCarburant.tsx
@@ -2,17 +2,17 @@ interface SelectCarburantProps {
   setCarburant: (i: number) => void;
 }
 
-export const SelectCarburant: React.FC<SelectCarburantProps> = ({setCarburant}) => {
+export const SelectCarburant: React.FC<SelectCarburantProps> = ({ setCarburant }) => {
   return (
     <div className="flex gap-5 justify-center">
       <select
         onChange={(value) => setCarburant(value.target.value as unknown as number)}
         className="w-[80vw] rounded-full bg-gray-200 p-1 flex justify-between pr-3"
       >
-        <option value={0}>Carburant 1</option>
-        <option value={1}>Carburant 2</option>
-        <option value={2}>Carburant 3</option>
+        <option value={0}>Méthane</option>
+        <option value={1}>Hydrogène</option>
+        <option value={2}>Kerosene</option>
       </select>
     </div>
   );
-}
+};
--- src/pages/Editeur/Editeur.tsx
+++ src/pages/Editeur/Editeur.tsx
@@ -10,7 +10,7 @@ export function Editeur() {
   const [carburant, setCarburant] = useState(0);
 
   const imageFusee = () => `/src/components/images/fusee${fusee}.png`;
-  const imageCarburant = () => `/src/components/images/carburant${carburant}.png`;
+  const imageCarburant = () => `/src/components/images/combustible${carburant}.png`;
 
   return (
     <div className="w-full h-screen bg-linear-to-b from-[#51B6FF] to-[#92FFFD]">
@@ -20,7 +20,7 @@ export function Editeur() {
       <SelectFusee setFusee={setFusee} />
       <SelectCarburant setCarburant={setCarburant} />
       <img className="h-[500px] absolute bottom-0 left-[500px] z-10" src={imageFusee()} />
-      <img className="h-[500px] absolute bottom-0 left-[900px] z-10" src={imageCarburant()} />
+      <img className="w-[200px] absolute bottom-[200px] left-[900px] z-10" src={imageCarburant()} />
       <img className="h-[150px] w-full absolute bottom-0 object-cover" src={imageSol} />
       <img className="h-[340px] absolute bottom-0 left-[300px]" src={imagePlateforme} />
     </div>
