2d2dc836a58bafcfb41fae6f618dfbebf04f4d36
Clément Gauthier <57112933+clem2004@users.noreply.github.com>
2025-02-08 17:44:09 -0500

    amelioration de la barre de co2

--- src/pages/Editeur/Editeur.tsx
+++ src/pages/Editeur/Editeur.tsx
@@ -15,7 +15,7 @@ export function Editeur() {
   return (
     <div className="w-full h-screen bg-linear-to-b from-[#51B6FF] to-[#92FFFD]">
       <div className="py-4">
-        <BarreCO2 limit="8T CO2 MAX" progres={0.5} />
+        <BarreCO2 limite={8} utilise={3} />
       </div>
       <SelectFusee setFusee={setFusee} />
       <SelectCarburant setCarburant={setCarburant} />
