5bcc52143f3a7e8a99d63f134e56f5664d448917
Clément Gauthier <57112933+clem2004@users.noreply.github.com>
2025-02-09 09:59:01 -0500

    ajout de la barre de co2

--- src/components/BarreCO2.tsx
+++ src/components/BarreCO2.tsx
@@ -2,18 +2,18 @@ import imageCO2 from "./images/co2.svg";
 
 interface BarreCO2Props {
   utilise: number;
-  limite: number;
+  addedCo2: number;
 }
 
-export function BarreCO2({ utilise, limite }: BarreCO2Props) {
+export function BarreCO2({ utilise, addedCo2 }: BarreCO2Props) {
   return (
     <div className="flex gap-5 justify-center">
       <img src={imageCO2} alt="CO2" className="w-12" />
       <div className="w-[80vw] rounded-full bg-gray-200 p-1 flex justify-between pr-3">
-        <div style={{ width: (utilise / limite) * 100 + "%" }} className="bg-[#910000] h-full rounded-full mr-3">
-          <p className="pl-3 text-white whitespace-nowrap">{utilise}T CO2</p>
+        <div style={{ width: (utilise / 5000) * 100 + "%" }} className="bg-[#910000] h-full rounded-full mr-3">
+          <p className="pl-3 text-white whitespace-nowrap">{Math.round(utilise)} T CO2</p>
         </div>
-        <p className="pl-3 text-[#910000] border-l-3 whitespace-nowrap">{limite}T CO2 MAX</p>
+        <p className="pl-3 text-[#910000] border-l-3 whitespace-nowrap">+ {addedCo2} T CO2</p>
       </div>
     </div>
   );
--- src/pages/simulation/Simulation.tsx
+++ src/pages/simulation/Simulation.tsx
@@ -3,13 +3,21 @@ import { BarreCO2 } from "../../components/BarreCO2";
 import imageTerre from "./images/terre.png";
 import { EARTH_RADIUS, Engine, Rocket } from "./simulator";
 
-const rockets: { mass: number; engines: Engine[]; diameter: number; dragCoefficient: number; burnTime: number }[] = [
+const rockets: {
+  mass: number;
+  engines: Engine[];
+  diameter: number;
+  dragCoefficient: number;
+  burnTime: number;
+  manufacturingCo2: number;
+}[] = [
   {
     mass: 2.6e6,
     engines: Array.from({ length: 4 }, () => new Engine(366, 1.86e6)),
     diameter: 8.4,
     dragCoefficient: 0.75,
     burnTime: 8 * 60,
+    manufacturingCo2: 3500,
   },
   {
     mass: 5e6,
@@ -17,6 +25,7 @@ const rockets: { mass: number; engines: Engine[]; diameter: number; dragCoeffici
     diameter: 9,
     dragCoefficient: 2.16,
     burnTime: 159,
+    manufacturingCo2: 3000,
   },
   {
     mass: 5.9e6,
@@ -24,6 +33,7 @@ const rockets: { mass: number; engines: Engine[]; diameter: number; dragCoeffici
     diameter: 5,
     dragCoefficient: 1.82,
     burnTime: 253,
+    manufacturingCo2: 2250,
   },
   {
     mass: 5.49e6,
@@ -31,27 +41,34 @@ const rockets: { mass: number; engines: Engine[]; diameter: number; dragCoeffici
     diameter: 3.7,
     dragCoefficient: 0.75,
     burnTime: 162,
+    manufacturingCo2: 1500,
   },
 ];
 
+const emissionsParKgConsomme: number[] = [2.75, 0.5, 3.1];
+
 export function Simulation() {
   const [position, setPosition] = useState({ x: 0, y: EARTH_RADIUS });
+  const [co2, setCo2] = useState(0);
 
   const params = new URLSearchParams(window.location.search);
   const rocket = rockets[parseInt(params.get("rocket") ?? "0")];
+  const emissions = emissionsParKgConsomme[parseInt(params.get("carburant") ?? "0")];
 
   useEffect(() => {
     const fusee = new Rocket(rocket.mass, rocket.engines, rocket.diameter, rocket.dragCoefficient, rocket.burnTime);
-    setInterval(() => {
+    const interval = setInterval(() => {
       for (let i = 0; i < 10; i++) fusee.step();
       setPosition(fusee.position);
+      setCo2((rocket.mass - fusee.mass) * emissions);
     }, 1000 / 10);
-  }, [rocket.burnTime, rocket.diameter, rocket.dragCoefficient, rocket.engines, rocket.mass]);
+    return () => clearInterval(interval);
+  }, [emissions, rocket.burnTime, rocket.diameter, rocket.dragCoefficient, rocket.engines, rocket.mass]);
 
   return (
     <div className="w-full h-screen bg-linear-to-b from-[#2c2c2c] to-[#000000]">
       <div className="py-4">
-        <BarreCO2 limite={8} utilise={3} />
+        <BarreCO2 addedCo2={rocket.manufacturingCo2} utilise={co2 / 1000} />
         <img
           src={imageTerre}
           className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-[70vh] rounded-full border-2 border-blue-500 rotate-[120deg]"
